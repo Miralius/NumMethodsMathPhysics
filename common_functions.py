e16bd67c64e192f11b87a2eee28b8f30a6c1bae9
Антон Фролов <F-Miralius@yandex.ru>
2021-04-12 13:20:51 +0400

    Первый коммит — без GUI и только схема Кранка-Николсона

---
+++ common_functions.py
@@ -0,0 +1,146 @@
+import os
+import matplotlib.pyplot as plt  # отрисовка графиков
+import numpy as np
+
+from crank_nicholson_scheme import *
+
+
+# noinspection SpellCheckingInspection
+def linspace(a, b, n):
+    h = (b - a) / n
+    return np.array([i * h for i in range(n + 1)])
+
+
+# Аналитическое решение
+def analytical_solution(x, t, alpha, c, d, time, length, number):
+    path = 'analytical_solutions/I=' + str(len(x) - 1) + ' K=' + str(len(t) - 1) + ' alpha=' + str(alpha) + ' c=' + str(
+        c) + ' D=' + str(d) + ' T=' + str(time) + ' l=' + str(length) + ' N=' + str(number)
+    if not os.path.isdir('analytical_solutions'):
+        os.mkdir('analytical_solutions')
+    if os.path.exists(path + '.npz'):
+        return np.load(path + '.npz')['arr_0']
+    solution = np.zeros((len(x), len(t)))
+    for k in range(len(t)):
+        for i in range(len(x)):
+            summa = 0
+            n = 1
+            while n <= number:
+                lambda_n = np.pi * (2 * n - 1) / (2 * length)
+                mu_n = (1 / c) * (alpha * lambda_n ** 2 + d)
+                summa += (2 * np.sin(lambda_n * x[i]) * (2 * (d - c * mu_n) * np.exp(-mu_n * t[k]) + mu_n * (2 * c - d *
+                                                                                                             time) *
+                                                         np.exp(-2 * t[k] / time) + d * (mu_n * time - 2))) / (
+                                     length * lambda_n * mu_n * c * (mu_n * time - 2))
+                n += 1
+            solution[i][k] = -summa + 1 - np.exp(-2 * t[k] / time)
+    np.savez_compressed(path, solution)
+    return solution
+
+
+def mean_norm_error(field1, field2):
+    return np.mean(abs(field1 - field2))
+
+
+def uniform_norm_error(field1, field2):
+    eps = 0
+    test = abs(field1 - field2)
+    for j in range(len(field1)):
+        if max(abs(field1[j] - field2[j])) > eps:
+            eps = max(abs(field1[j] - field2[j]))
+    return eps
+
+
+def root_mean_square_norm_error(field1, field2):
+    eps = 0
+    for i in range(len(field1)):
+        for j in range(len(field1[0])):
+            eps += (field1[i][j] - field2[i][j]) ** 2
+    return np.sqrt(eps / (len(field1) * len(field1[0])))
+
+
+def find_number(alpha, c, d, time, length):
+    size = 100
+    x = linspace(0, length, size)  # разбиение интервала длины
+    t = linspace(0, time, size)  # разбиение интервала времени
+    number = 1
+    while True:
+        field = analytical_solution(x, t, alpha, c, d, time, length, number)
+        field2 = analytical_solution(x, t, alpha, c, d, time, length, number + 1)
+        maximum = uniform_norm_error(field, field2)
+        print(str(maximum))
+        print("n = " + str(number))
+        if maximum < 10 ** (-9):
+            break
+        number += 1
+    return number
+
+
+def get_numerical_experiments(i, k, alpha, c, d, time, length, number, hx_rate, ht_rate, method, norm_error):
+    results = np.zeros((len(i), 5), dtype=float)
+    for j in range(len(i)):
+        results[j][0] = i[j]
+        results[j][1] = k[j]
+        x = linspace(0, length, i[j])
+        t = linspace(0, time, k[j])
+        field1 = analytical_solution(x, t, alpha, c, d, time, length, number)
+        field2 = method(x, t, alpha, c, d)
+        eps = norm_error(field1, field2)
+        results[j][2] = eps
+        x = linspace(0, length, hx_rate * i[j])
+        t = linspace(0, time, ht_rate * k[j])
+        field1 = analytical_solution(x, t, alpha, c, d, time, length, number)
+        field2 = method(x, t, alpha, c, d)
+        eps2 = norm_error(field1, field2)
+        results[j][3] = eps2
+        results[j][4] = eps / eps2
+    return results
+
+
+# Отрисовка одномерных графиков
+def plot(x, functions, names, label, name, axis_values=None):
+    xy = plt.subplot()
+    xy.grid(which='major', color='k')
+    xy.minorticks_on()
+    xy.grid(which="minor", color='gray', linestyle=':')
+    i = 0
+    while i < len(functions):
+        xy.plot(x, functions[i], label=names[i])
+        i += 1
+    xy.set_xlabel(label)
+    xy.set_ylabel("u(x,t), г/см³")
+    if axis_values is not None:
+        plt.axis(axis_values)
+    xy.legend()
+    xy.set_title(name, fontsize=9)
+    plt.show()
+
+
+# Отрисовка одномерных графиков
+def plot_test(x, functions, names, label, name, axis_values=None):
+    xy = plt.subplot()
+    xy.grid(which='major', color='k')
+    xy.minorticks_on()
+    xy.grid(which="minor", color='gray', linestyle=':')
+    i = 0
+    while i < len(functions):
+        xy.plot(x[i], functions[i], label=names[i])
+        i += 1
+    xy.set_xlabel(label)
+    xy.set_ylabel("u(x,t), г/см³")
+    if axis_values is not None:
+        plt.axis(axis_values)
+    xy.legend()
+    xy.set_title(name, fontsize=9)
+    plt.show()
+
+
+# Отрисовка двумерных графиков
+def plot_2d(field, label):  # field -результат , Label - надпись
+    my_map = plt.get_cmap("plasma")  # цветовая схема
+    plt.set_cmap(my_map)  # применение схемы к фигуре графика
+    plt.figure(figsize=(10, 10))  # создание фигуры с размером 1000*1000 пикселей
+
+    plt.imshow(field)  # наложение на график изобр. ампл. поля
+    plt.title(label, fontsize=9)  # заголовок
+    plt.colorbar()
+    plt.show()
---
+++ main.py
@@ -0,0 +1,89 @@
+from common_functions import *
+
+if __name__ == '__main__':
+    length = 10  # см — длина трубки
+    I_ = 100  # кол-во интервалов разбиения по длине
+    time = 150  # с — время наблюдения
+    K = 100  # кол-во интервалов разбиения по времени
+    alpha = 0.24  # см²/с — коэффициент диффузии
+    c = 0.8  # коэффициент пористости
+    d = 0.002  # 1/с — коэффициент пропорциональности концентрации растворенного вещества
+    x = linspace(0, length, I_)  # разбиение интервала длины
+    t = linspace(0, time, K)  # разбиение интервала времени
+    #  number = find_number(alpha, c, d, time, length)
+    number = 650
+    field = analytical_solution(x, t, alpha, c, d, time, length, number)
+
+    x_0, = np.where(x >= 0)
+    x_1, = np.where(x >= 3)
+    x_2, = np.where(x >= 6)
+    x_3, = np.where(x >= 9)
+    t_0, = np.where(t >= 0)
+    t_1, = np.where(t >= 30)
+    t_2, = np.where(t >= 75)
+    t_3, = np.where(t >= 140)
+    plot_2d(field, "Аналитическое решение u(x,t)")
+    plot(t, np.array([field[x_0[0]], field[x_1[0]], field[x_2[0]], field[x_3[0]]]),
+         np.array(["x = " + str(x[x_0[0]]), "x = " + str(x[x_1[0]]), "x = " + str(x[x_2[0]]), "x = " + str(x[x_3[0]])]),
+         "t, с", "Изменение аналитического решения u(x,t) по времени")
+    plot(x, np.array([field.T[t_0[0]], field.T[t_1[0]], field.T[t_2[0]], field.T[t_3[0]]]),
+         np.array(["t = " + str(t[t_0[0]]), "t = " + str(t[t_1[0]]), "t = " + str(t[t_2[0]]), "t = " + str(t[t_3[0]])]),
+         "x, см", "Изменение аналитического решения u(x,t) по координате")
+
+    field_C_N = crank_nicholson_solve(x, t, alpha, c, d)
+    plot_2d(field_C_N, "Решения для схемы Кранка-Николсона u(x,t)")
+    plot(t, np.array([field_C_N[x_0[0]], field_C_N[x_1[0]], field_C_N[x_2[0]], field_C_N[x_3[0]]]),
+         np.array(["x = " + str(x[x_0[0]]), "x = " + str(x[x_1[0]]), "x = " + str(x[x_2[0]]), "x = " + str(x[x_3[0]])]),
+         "t, с", "Изменение концентрации вещества u(x,t) по времени")
+    plot(x, np.array([field_C_N.T[t_0[0]], field_C_N.T[t_1[0]], field_C_N.T[t_2[0]], field_C_N.T[t_3[0]]]),
+         np.array(["t = " + str(t[t_0[0]]), "t = " + str(t[t_1[0]]), "t = " + str(t[t_2[0]]), "t = " + str(t[t_3[0]])]),
+         "x, см", "Изменение концентрации вещества u(x,t) по координате")
+
+    plot(t, np.array([field[x_0[0]], field[x_1[0]], field[x_2[0]], field[x_3[0]],
+                      field_C_N[x_0[0]], field_C_N[x_1[0]], field_C_N[x_2[0]], field_C_N[x_3[0]]]),
+         np.array(["x_a = " + str(x[x_0[0]]), "x_a = " + str(x[x_1[0]]), "x_a = " + str(x[x_2[0]]),
+                   "x_a = " + str(x[x_3[0]]), "x = " + str(x[x_0[0]]), "x = " + str(x[x_1[0]]), "x = " + str(x[x_2[0]]),
+                   "x = " + str(x[x_3[0]])]), "t, с", "Изменение аналитического и численного решений u(x,t) по времени")
+    plot(x, np.array([field.T[t_0[0]], field.T[t_1[0]], field.T[t_2[0]], field.T[t_3[0]], field_C_N.T[t_0[0]],
+                      field_C_N.T[t_1[0]], field_C_N.T[t_2[0]], field_C_N.T[t_3[0]]]), np.array(
+        ["t_a = " + str(t[t_0[0]]), "t_a = " + str(t[t_1[0]]), "t_a = " + str(t[t_2[0]]), "t_a = " + str(t[t_3[0]]),
+         "t = " + str(t[t_0[0]]), "t = " + str(t[t_1[0]]), "t = " + str(t[t_2[0]]), "t = " + str(t[t_3[0]])]), "x, см",
+         "Изменение аналитического и численного решений u(x,t) по координате")
+
+    x_1, = np.where(x >= 5)
+    I_, K = 10, 10
+    x1 = linspace(0, length, I_)  # разбиение интервала длины
+    t1 = linspace(0, time, K)  # разбиение интервала времени
+    field_C_N_1 = crank_nicholson_solve(x1, t1, alpha, c, d)
+    t__1, = np.where(t1 >= 30)
+    x__1, = np.where(x1 >= 5)
+
+    I_, K = 100, 100
+    x2 = linspace(0, length, I_)  # разбиение интервала длины
+    t2 = linspace(0, time, K)  # разбиение интервала времени
+    field_C_N_2 = crank_nicholson_solve(x2, t2, alpha, c, d)
+    t__2, = np.where(t2 >= 30)
+    x__2, = np.where(x2 >= 5)
+
+    I_, K = 1000, 1000
+    x3 = linspace(0, length, I_)  # разбиение интервала длины
+    t3 = linspace(0, time, K)  # разбиение интервала времени
+    field_C_N_3 = crank_nicholson_solve(x3, t3, alpha, c, d)
+    t__3, = np.where(t3 >= 30)
+    x__3, = np.where(x3 >= 5)
+
+    plot_test(np.array([x, x1, x2, x3], dtype=object), np.array([field_C_N.T[t_1[0]], field_C_N_1.T[t__1[0]],
+                                                                 field_C_N_2.T[t__2[0]], field_C_N_3.T[t__3[0]]],
+                                                                dtype=object),
+              np.array(["t_a", "t_" + str(len(t1) - 1), "t_" + str(len(t2) - 1), "t_" + str(len(t3) - 1)]), "x, см",
+              "Тест сгущающихся сеток для t = " + str(t1[t__1[0]]))
+    plot_test(np.array([t, t1, t2, t3], dtype=object), np.array([field_C_N[x_1[0]], field_C_N_1[x__1[0]],
+                                                                 field_C_N_2[x__2[0]], field_C_N_3[x__3[0]]],
+                                                                dtype=object),
+              np.array(["x_a", "x_" + str(len(x1) - 1), "x_" + str(len(x2) - 1), "x_" + str(len(x3) - 1)]), "t, с",
+              "Тест сгущающихся сеток для x = " + str(x1[x__1[0]]))
+
+    grid_x = np.array([10, 20, 40, 80, 160, 320, 640, 1280])
+    grid_t = np.array([10, 20, 40, 80, 160, 320, 640, 1280])
+    print(get_numerical_experiments(
+        grid_t, grid_x, alpha, c, d, time, length, number, 2, 2, crank_nicholson_solve, root_mean_square_norm_error))
